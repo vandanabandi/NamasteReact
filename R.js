b688a362dcbf112625a4eabfc2db26ac94386db9
Vandana <bvandana43@gmail.com>
2024-12-26 17:05:06 +0530

    Episode 02

--- R.js
+++ R.js
@@ -1,3 +1,6 @@
-        const heading1=React.createElement("h1",{id:"heading"},"Hi from React");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1=React.createElement("h1",{id:"heading"},"Hi from React");
         const root1=ReactDOM.createRoot(document.getElementById("root1"));
         root1.render(heading1);
