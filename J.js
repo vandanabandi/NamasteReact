ac2b6f5f597eeb0efd920a6813b2fa15cdf34e98
Vandana <bvandana43@gmail.com>
2024-12-26 14:58:10 +0530

    Episode 01

---
+++ J.js
@@ -0,0 +1,4 @@
+const heading= document.createElement("h2");
+heading.innerHTML="Hi from Javascript";
+const root=document.getElementById("root");
+root.appendChild(heading);
---
+++ R.js
@@ -0,0 +1,3 @@
+        const heading1=React.createElement("h1",{id:"heading"},"Hi from React");
+        const root1=ReactDOM.createRoot(document.getElementById("root1"));
+        root1.render(heading1);
